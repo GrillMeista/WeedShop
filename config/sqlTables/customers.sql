26245d29b03c9cc4a087c04905519de0f8e5e633
Jonas <32982865+GrillMeista@users.noreply.github.com>
2017-11-20 20:11:08 +0100

    Create customers.sql

---
+++ config/sqlTables/customers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customers (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+email_adress VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
+reg_date TIMESTAMP
+)
